066faf88fa7b69c6a01f5aeecde423ddff652bdc
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-07-30 18:50:36 -0500

    update

--- main_market.py
+++ main_market.py
@@ -6,4 +6,15 @@ import mysql.connector
 import sql_config as sc
 
 def main():
+    # Actually trying to enter into the sql server
+
+    try:
+        connection = mysql.connector.connect(user=sc.user, password = sc.password, host = sc.host, database = "grocermarket")
+        
+        cursor = connection.cursor()
     
+    except mysql.connector.Error as mce:
+        print(mce.msg)
+        return
+    
+    logging.basicConfig(filename = "grocerLog.log", level = logging.DEBUG, format='%(asctime)s :: %(message)s')
