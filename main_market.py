b038c4bc20a301bd0e4ebe833a82c5fe87e4e01c
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-07-30 18:39:17 -0500

    update

---
+++ main_market.py
@@ -0,0 +1,9 @@
+import re
+import logging
+
+#### SET UP SQL CONNECTION HERE ##############################################################################################################################################
+import mysql.connector
+import sql_config as sc
+
+def main():
+    
---
+++ sql_config.py
@@ -0,0 +1,3 @@
+user = "root"
+password = "rev519@07A#58B"
+host = "127.0.0.1"
