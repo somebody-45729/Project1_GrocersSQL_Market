9f7684886e91d280258ce603124e8f1d10d0fef7
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-01 20:28:43 -0500

    update

--- main_market.py
+++ main_market.py
@@ -6,15 +6,20 @@ import mysql.connector
 import sql_config as sc
 
 def main():
-    # Actually trying to enter into the sql server
+    # Actually trying to connect the sql server
 
     try:
-        connection = mysql.connector.connect(user=sc.user, password = sc.password, host = sc.host, database = "grocermarket")
-        
+        connection = mysql.connector.connect(user=sc.user, password = sc.password, host = sc.host, database = sc.database)    
         cursor = connection.cursor()
     
     except mysql.connector.Error as mce:
         print(mce.msg)
         return
+    
+    except Exception as e:
+        print("ERROR: Exiting program")
+        return
+
+    # Connection works (8/1/22 8:28 PM)
     
     logging.basicConfig(filename = "grocerLog.log", level = logging.DEBUG, format='%(asctime)s :: %(message)s')
--- sql_config.py
+++ sql_config.py
@@ -1,3 +1,5 @@
 user = "root"
 password = "rev519@07A#58B"
-host = "127.0.0.1"
+host = "localhost"
+port = 3306
+database = "grocermarket"
