a716f20d2546f06855cabf07247faea147f40ee3
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-02 20:36:38 -0500

    update

--- main_connection.py
+++ main_connection.py
@@ -14,7 +14,7 @@ def main():
     
     except mysql.connector.Error as mce:
         print(mce.msg)
-        return connection
+        return
     
     except Exception as e:
         print("ERROR: Exiting program")
@@ -24,4 +24,24 @@ def main():
     
     logging.basicConfig(filename = "grocerLog.log", level = logging.DEBUG, format='%(asctime)s :: %(message)s')
 
-    print(connection)
+    # MENU FOR SELECTION
+
+    choice = 0
+
+    while (choice <= 4): # NEED TO ADD IN A MENU SELECTION
+        # MENU OPTIONS (CRUD)
+        print ("\nSELECT FROM THE FOLLOWING OPTIONS:")
+        print("\t1) CREATE")
+        print("\t2) READ")
+        print("\t3) UPDATE")
+        print("\t4) DELETE")
+        print("\t5) EXIT")
+
+        try:
+            choice = int(input("ENTER YOUR CHOICE (1 - 5): "))
+        except ValueError:
+            print("Not a valid integer, please try again!")
+            logging.info("User has not entered integer, retrying again . . .")
+
+
+
