86fd697e02e3744207a288970a6f6bf2e737375c
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-03 19:40:34 -0500

    update

--- testConnect.py
+++ testConnect.py
@@ -4,15 +4,39 @@ import logging # need logging of events
 import mysql.connector
 import sql_config as sc
 
-conn = mysql.connector.connect(
-    host = sc.host,
-    database = sc.database,
-    user = sc.user,
-    password = sc.password
-)
+def main():
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM customers")
+    conn = mysql.connector.connect(
+        host = sc.host,
+        database = sc.database,
+        user = sc.user,
+        password = sc.password
+    )
 
-for row in cursor:
-    print(row)
+    cursor = conn.cursor()
+    choice = 0
+
+    while (choice <= 4):
+
+        print("1. INSERT")
+        print("2. READ")
+        print("3. UPDATE")
+        print("4. DELETE")
+        print("5. EXIT")
+
+        choice = int(input("Enter your choice: "))
+        if (choice == 1):
+            insert(conn, cursor)
+        elif (choice == 2):
+            read(cursor)
+        elif (choice == 3):
+            update(conn, cursor)
+        elif (choice == 4):
+            delete(conn, cursor)
+        else:
+            exit()
+
+    
+    # call main
+    if __name__ == "__main__":
+        main()
