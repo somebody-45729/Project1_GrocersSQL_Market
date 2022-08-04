9a7bd7e5a4d2fa6172a5734185c8115815783666
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-04 18:18:31 -0500

    update

--- testConnect.py
+++ testConnect.py
@@ -68,7 +68,7 @@ def read_history(cursor):
     print("dt      orderid        passkey")
     print("------------------------------")
     for item in fetch:
-        print(str(item[0])+"    "+item[1]+"    "+item[2])
+        print((item[0])+"    "+(item[1])+"    "+(item[2]))
     logging.info("SHOWED ALL AVAILABLE QUERIES WITHIN TABLE orderHistory")
 
 
