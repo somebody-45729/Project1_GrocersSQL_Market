c6bc9d52869a4209a5a50987471e23577c04b056
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-04 23:45:06 -0500

    update

--- testConnect.py
+++ testConnect.py
@@ -9,44 +9,7 @@ from tkinter import *
 from tkinter import messagebox
 from subprocess import call
 
-
-def input():
-    mysqlDB = mysql.connector.connect(host="localhost", user="root", password=sc.password, database="grocermarket")
-    cursor = mysqlDB.cursor()
-    usename = un.get()
-    password = pw.get()
-
-    sql = "SELECT * FROM userAdmin WHERE named = %s AND passW = %s"
-    cursor.execute(sql, [(usename), (password)])
-    r = cursor.fetchall()
-
-    if r:
-        messagebox.showinfo("","Login Successful")
-        root.destroy()
-        return True
-    else:
-        messagebox.showinfo(""," INCORRECT USERNAME AND PASSWORD")
-        return False
-
-root = Tk()
-root.title("Login (test)")
-root.geometry("400x300")
-global un
-global pw
-
-Label(root, text="USERNAME").place(x = 30, y = 30)
-Label(root, text="PASSWORD").place(x = 30, y = 60)
-
-un = Entry(root)
-un.place(x=140, y=30)
-
-pw = Entry(root)
-pw.place(x=140, y=60)
-pw.config(show="*")
-
-Button(root, text="Login", command=input, height = 5, width = 25).place(x=30, y=130)
-
-root.mainloop()
+from login import *
 
 
 ############################ CREATE (AKA: INSERT) ###############################################################################################################################
@@ -191,6 +154,8 @@ def delete_history(conn, cur):
 
 
 ############################################# MAIN CONNECTION SECTION ##########################################################################################################
+input() # calling the login function
+
 def main(): 
     
     conn = mysql.connector.connect(
