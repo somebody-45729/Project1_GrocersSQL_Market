0b4cf6732a8b19f609aeeb2d1a3b409f18778e40
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-08-04 23:17:04 -0500

    update

---
+++ login.py
@@ -0,0 +1,45 @@
+from os import uname
+import pwd
+import mysql.connector
+from tkinter import *
+from tkinter import messagebox
+from subprocess import call
+
+
+def input():
+    mysqlDB = mysql.connector.connect(host="localhost", user="root", password="", database="grocermarket")
+    cursor = mysqlDB.cursor()
+    usename = un.get()
+    password = pw.get()
+
+    sql = "SELECT * FROM userAdmin WHERE named = %s AND passW = %s"
+    cursor.execute(sql, [(usename), (password)])
+    r = cursor.fetchall()
+
+    if r:
+        messagebox.showinfo("","Login Successful")
+        root.destroy()
+        return True
+    else:
+        messagebox.showinfo(""," INCORRECT USERNAME AND PASSWORD")
+        return False
+
+root = Tk()
+root.title("Login (test)")
+root.geometry("500x500")
+global un
+global pw
+
+Label(root, text="USERNAME").place(x = 30, y = 30)
+Label(root, text="PASSWORD").place(x = 30, y = 60)
+
+un = Entry(root)
+un.place(x=140, y=30)
+
+pw = Entry(root)
+pw.place(x=140, y=60)
+pw.config(show="*")
+
+Button(root, text="Login", command=Ok, height = 5, width = 25).place(x=30, y=130)
+
+root.mainloop()
